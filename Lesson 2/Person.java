b787c9816dfc739cd639db13bede64adb8e6f49b
Мальцев Антон <toxamg@live.ru>
2020-03-03 22:24:24 +0300

    ДА класс Person

---
+++ Lesson 2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+	
+	String name = "Anton";
+	String sex = "Male";
+	float height = 1.75f;
+	int weight = 80; 
+	int age = 40;
+	
+	void walk() {
+		System.out.println("Вы идете");
+	}
+
+	boolean isSit() {
+		return false;
+	}
+
+	void run() {
+		System.out.println("Вы пробежали 5 км.");
+	}
+
+	String speak() {
+		return "Java";
+	}
+
+	boolean isLearnJava() {
+		return true;
+	}
+}
